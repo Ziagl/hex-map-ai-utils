44f962c372243697892c9bc9e830e309315dfeec
Ziagl <office@ziegelwanger-edv.at>
2025-03-01 11:59:29 +0100

    version 0.0.1

---
+++ HexMapAIUtils.Tests/AIUtilsTests.cs
@@ -0,0 +1,53 @@
+﻿using com.hexagonsimulations.HexMapBase.Geometry.Hex;
+
+namespace HexMapAIUtils.Tests
+{
+    [TestClass]
+    public sealed class AIUtilsTests
+    {
+        [TestMethod]
+        public void TestFindPlayerStartingPositions()
+        {
+            List<List<int>> map = new List<List<int>>
+            {
+                new List<int> { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                new List<int> { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                new List<int> { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                new List<int> { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                new List<int> { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                new List<int> { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                new List<int> { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                new List<int> { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                new List<int> { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                new List<int> { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
+            };
+            List<OffsetCoordinates> startPositions = AIUtils.FindPlayerStartingPositions(2, map);
+            Assert.AreEqual(2, startPositions.Count);
+            Assert.IsTrue(Distance(startPositions[0], startPositions[1]) > 4.0);
+
+            new List<List<int>>
+            {
+                new List<int> { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                new List<int> { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                new List<int> { 0, 0, 1, 1, 1, 1, 1, 0, 0, 1 },
+                new List<int> { 1, 0, 0, 1, 1, 1, 0, 0, 0, 1 },
+                new List<int> { 1, 1, 1, 1, 1, 1, 0, 0, 1, 1 },
+                new List<int> { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                new List<int> { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                new List<int> { 1, 1, 1, 1, 0, 0, 1, 1, 1, 1 },
+                new List<int> { 1, 1, 1, 1, 0, 0, 0, 1, 1, 1 },
+                new List<int> { 1, 1, 1, 1, 1, 0, 0, 1, 1, 1 }
+            };
+            startPositions = AIUtils.FindPlayerStartingPositions(5, map);
+            Assert.AreEqual(5, startPositions.Count);
+            Assert.IsTrue(Distance(startPositions[0], startPositions[1]) > 3.0);
+            Assert.IsTrue(Distance(startPositions[2], startPositions[3]) > 3.0);
+            Assert.IsTrue(Distance(startPositions[1], startPositions[4]) > 3.0);
+        }
+
+        private double Distance(OffsetCoordinates a, OffsetCoordinates b)
+        {
+            return Math.Sqrt(Math.Pow(a.x - b.x, 2) + Math.Pow(a.y - b.y, 2));
+        }
+    }
+}
---
+++ HexMapAIUtils/AIUtils.cs
@@ -0,0 +1,54 @@
+﻿using com.hexagonsimulations.HexMapBase.Geometry.Hex;
+
+namespace HexMapAIUtils;
+
+public class AIUtils
+{
+    /// <summary>
+    /// Finds starting points for players on a given map. The starting points are
+    /// equally distributed across the map.
+    /// </summary>
+    /// <param name="numPositions">Number of player starting positions needed.</param>
+    /// <param name="map">A map of binary values meaning 0 unpassable and >0 passable.</param>
+    /// <returns>Returns a list of computed starting positions.</returns>
+    public static List<OffsetCoordinates> FindPlayerStartingPositions(int numPositions, List<List<int>> map)
+    {
+        List<int[]> passablePositions = new List<int[]>();
+        for (int i = 0; i < map.Count(); i++)
+        {
+            for (int j = 0; j < map[i].Count; j++)
+            {
+                if (map[i][j] != 0)
+                {
+                    passablePositions.Add(new int[] { i, j });
+                }
+            }
+        }
+
+        // K-Means algorithm
+        double[][] centroids = Utils.InitializeCentroids(passablePositions.ToArray(), numPositions);
+        double[][] oldCentroids;
+        int[] labels = new int[passablePositions.Count];
+
+        do
+        {
+            oldCentroids = (double[][])centroids.Clone();
+
+            for (int i = 0; i < passablePositions.Count; i++)
+            {
+                labels[i] = Utils.FindClosestCentroid(passablePositions[i], centroids);
+            }
+
+            centroids = Utils.UpdateCentroids(passablePositions.ToArray(), labels, numPositions);
+
+        } while (!Utils.AreCentroidsEqual(oldCentroids, centroids));
+
+        List<OffsetCoordinates> startPositions = new List<OffsetCoordinates>();
+        for (int i = 0; i < numPositions; i++)
+        {
+            startPositions.Add(new OffsetCoordinates((int)centroids[i][0], (int)centroids[i][1]));
+        }
+
+        return startPositions;
+    }
+}
